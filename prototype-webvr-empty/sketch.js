bd6a7e2058b42a5d37e575f3e057e8308094170c
Zeyao <zeyao.li.315@gmail.com>
2019-04-24 17:54:50 +0800

    Add empty files for workshop attendees to download

---
+++ prototype-webvr-empty/sketch.js
@@ -0,0 +1,21 @@
+let world;
+
+function preload() {
+
+}
+
+
+function setup() {
+  // no canvas needed
+  noCanvas();
+
+  // construct the A-Frame world
+  // this function requires a reference to the ID of the 'a-scene' tag in our HTML document
+  world = new World('VRScene');
+}
+
+
+
+function draw() {
+
+}
